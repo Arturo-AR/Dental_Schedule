f7775b3a350b64ca8a5728eb2ca531720c2a0f89
Arturo Anguiano <arturoanguiano@MacBook-Pro-de-Arturo.local>
2023-07-14 15:10:56 -0500

    Constants files added

---
+++ lib/src/constants/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+/* -- LIST OF ALL COLORS -- */
+
+const aPrimaryColor = Color(0xFF33C9DD);
+const aSecondaryColor = Color(0xFF272727);
+const aAccentColor = Color(0xFFFD1B1B);
+
+const aWhiteColor = Colors.white;
+const aDarkColor = Color(0xff272727);
+const aCardBgColor = Color(0xFFF7F5F1);
+
+// -- SOCIAL Button COLORS
+const aGoogleBgColor = Color(0xFFDFEFFF);
+const aGoogleForegroundColor = Color(0xFF167EE6);
+const aFacebookBgColor = Color(0xFF1877F2);
+
+// -- ON-BOARDING COLORS
+const aOnBoardingPage1Color = Colors.white;
+const aOnBoardingPage2Color = Color(0xfffddcdf);
+const aOnBoardingPage3Color = Color(0xffffdcbd);
